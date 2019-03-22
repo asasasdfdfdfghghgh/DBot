658282344a5765dc68f395277b0f79ce4b513e66
AhmedMagdyi <43391576+AhmedMagdyi@users.noreply.github.com>
2019-03-23 00:22:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,18 @@
  const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "$";
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Dbot | D7ommy`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
+
 client.on("message", message => {
 
             if (message.content.startsWith(prefix + "bc")) {
@@ -16,16 +28,7 @@ client.on("message", message => {
 });
 
 
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : i1Suhaib`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Dbot | D7ommy`,"http://twitch.tv/S-F")
-client.user.setStatus("dnd")
-});
+
 
 
 
